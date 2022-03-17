8736ccf5e25e716622c1b39e5f4b9030960af14f
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2022-03-17 14:30:58 +0100

    updating and inserting values to tables

--- project.sql
+++ project.sql
@@ -8,9 +8,10 @@ SHOW databases;
 USE libery;
 -- create book table
 CREATE TABLE book(
-`book_id` int auto_increment NOT NULL PRIMARY KEY,
+`id` int auto_increment NOT NULL PRIMARY KEY,
 `title` VARCHAR(60) NOT NULL,
-`author` VARCHAR(60),
+`author` int,
+ FOREIGN KEY (id) REFERENCES author(id),
 `publication_year` DATE
 );
 -- create author table
@@ -26,5 +27,49 @@ CREATE TABLE client(
 `last_nam` VARCHAR(20),
 `joined_date` DATE
 ); 
+-- loan table
+CREATE TABLE loan(
+`id` int auto_increment NOT NULL PRIMARY KEY,
+`book` int,
+FOREIGN KEY(id) REFERENCES book(id),
+`clinet` int,
+FOREIGN KEY(id) REFERENCES client(id),
+`loan_date` DATE
+);
 -- view tables
 SHOW TABLES;
+-- solution for  Error code 1452 - Cannot add or update a child row: a foreign key constraint fails
+set foreign_key_checks=0;
+
+--- inserting to tables
+INSERT INTO author VALUES(
+5,
+"Murwan",
+"Eisa"
+);
+INSERT INTO book(id, title,author, publication_year) VALUES(
+5,
+"React JS book",
+4,
+"2020/10/18"
+);
+
+INSERT INTO client VALUES(
+5,
+"Amira",
+"Abdallah",
+"2021/4/2"
+);
+
+INSERT INTO loan VALUES(
+5,
+4,
+4,
+"2021/4/5"
+); 
+
+-- show tables value
+SELECT * FROM book;
+SELECT * FROM author;
+SELECT * FROM loan;
+SELECT * FROM client;
